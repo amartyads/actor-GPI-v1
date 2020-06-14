0331ebbd25e54962a0cf7fe26dcb100ffb4ddbb0
Amartya Das Sharma <amartya.das-sharma@tum.de>
2020-06-14 19:44:48 +0200

    added helloworld to test

---
+++ hellog3.cpp
@@ -0,0 +1,23 @@
+#include "../local/include/GASPI.h"
+#include "../local/include/GASPI_Ext.h"
+#include "gpi-utils.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ASSERT(ec) gpi_util::success_or_exit(__FILE__,__LINE__,ec)
+
+int main(int argc, char *argv[])
+{
+	gaspi_rank_t rank, num;
+	gaspi_return_t ret;
+
+	ASSERT( gaspi_proc_init(GASPI_BLOCK) );
+
+	ASSERT( gaspi_proc_rank(&rank));
+	ASSERT( gaspi_proc_num(&num) );
+	gaspi_printf("Hello from ran %d of %d\n", rank, num);
+
+	ASSERT( gaspi_proc_term(GASPI_BLOCK) );
+
+	return EXIT_SUCCESS;
+}
