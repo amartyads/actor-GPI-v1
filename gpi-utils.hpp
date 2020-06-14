dda83d66c4a36fc34435d0b36ac3f8bea4c8df0e
Amartya Das Sharma <amartya.das-sharma@tum.de>
2020-06-14 18:15:38 +0200

    utils added

---
+++ gpi-utils.hpp
@@ -0,0 +1,39 @@
+#ifndef ACTORGPI_UTILS_HPP
+#define ACTORGPI_UTILS_HPP
+
+
+#include "../local/include/GASPI.h"
+#include "../local/include/GASPI_Ext.h"
+#include <stdlib.h>
+
+namespace gpi_util
+{
+	void success_or_exit(const char* file, const int line, const int ec)
+	{
+		if(ec!=GASPI_SUCCESS)
+		{
+			gaspi_printf("Assertions failed in $s[%i]:%d\n",file,line,ec);
+			exit(EXIT_FAILURE);
+		}
+	}
+	void wait_if_queue_full ( const gaspi_queue_id_t queue_id
+                        , const gaspi_number_t request_size
+                        )
+	{
+	  gaspi_number_t queue_size_max;
+	  gaspi_number_t queue_size;
+	 
+	  success_or_exit (__FILE__, __LINE__, gaspi_queue_size_max (&queue_size_max));
+	  success_or_exit (__FILE__, __LINE__, gaspi_queue_size (queue_id, &queue_size));
+	 
+	  if (queue_size + request_size >= queue_size_max)
+	    {
+	      success_or_exit (__FILE__, __LINE__, gaspi_wait (queue_id, GASPI_BLOCK));
+	    }
+	}
+
+}
+
+
+
+#endif
